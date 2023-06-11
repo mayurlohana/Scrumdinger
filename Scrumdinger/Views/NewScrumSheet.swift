8313cc172c659ef987e667b15f30d04bc08f23f3
Mayur <mayurlohana121@gmail.com>
2023-06-12 01:23:12 +0500

    Added new files and made some changes in older files.

---
+++ Scrumdinger/Views/NewScrumSheet.swift
@@ -0,0 +1,41 @@
+//
+//  NewScrumSheet.swift
+//  Scrumdinger
+//
+//  Created by Mayur Lohana on 6/11/23.
+//
+
+import SwiftUI
+
+struct NewScrumSheet: View {
+    @State private var newScrum = DailyScrum.emptyScrum
+    @Binding var isPresentingNewScrumView: Bool
+    @Binding var scrums: [DailyScrum]
+    
+    var body: some View {
+        NavigationStack {
+            DetailEditView(scrum: $newScrum)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Dismiss") {
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Add") {
+                            scrums.append(newScrum  )
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                }
+        }
+    }
+}
+
+
+struct NewScrumSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        NewScrumSheet(isPresentingNewScrumView: .constant(true),
+                      scrums: .constant(DailyScrum.sampleData))
+    }
+}
